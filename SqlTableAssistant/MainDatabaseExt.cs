d4ddd7047861b74b9ed058d4d1891ea68900ea54
Gennady <ltrg67@gmail.com>
2020-07-13 19:13:50 +0300

    Исправление бага

--- SqlTableAssistant/MainDatabaseExt.cs
+++ SqlTableAssistant/MainDatabaseExt.cs
@@ -11,8 +11,8 @@ namespace SqlTableAssistant
     {
         public static string InsertString(this IMainDatabase db, string table_name, Dictionary<string, object> query_params)
         {
-            string param1 = string.Join(",", query_params.Select((key, val) => key).ToArray());
-            string param2 = string.Join(",", query_params.Select((key, val) => $"@{key}").ToArray());
+            string param1 = string.Join(",", query_params.Select(o => o.Key).ToArray());
+            string param2 = string.Join(",", query_params.Select(o => $"@{o.Key}").ToArray());
             StringBuilder sb = new StringBuilder();
             sb.Append("INSERT INTO ");
             sb.Append(table_name);
