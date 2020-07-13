b5ad04df5f3c31ae410f614ad63cdacd6df6e5c7
Gennady <ltrg67@gmail.com>
2020-07-13 19:13:31 +0300

    Метод расширения для DateTime

---
+++ SqlTableAssistant/TimeExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlTableAssistant
+{
+    public static class TimeExtensions
+    {
+        public static string ToStringDatabase(this DateTime dateTime) => dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+    }
+}
